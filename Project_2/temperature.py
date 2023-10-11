e9767cb41c9cbd21a923f44ffa6c32906b3e60d3
Kuba Ornatek <kubaornatek@Kubas-MacBook-Pro.local>
2023-10-11 11:44:45 +0200

    temperature

---
+++ Project_2/temperature.py
@@ -0,0 +1,12 @@
+temp = int(input("Podaj temperature w stopniach Celsjusza: "))
+
+if (temp <= -10):
+	print("Ło panie, ale mróz!")
+elif (temp <= 0):
+    print("Chłodnawoooo...")
+elif (temp <= 15):
+	print("Rzeźko")
+elif (temp <= 25):
+    print("Jak ta lala")
+else:
+	print("Ło panie, jaki goronc!")
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # UW_Python_Course
-Small and Big Projects for class
+#### Small and Big Projects for class
+
+## Project_1:
+	1. Monte Carlo 
+
+## Project_2
+	1. randomRevesedList
+	2. temperature
