1d072c94007d56c8cfe1b86e601ade6da26c4907
Kuba Ornatek <kuba.ornatek@gmail.com>
2024-01-15 16:56:54 +0100

    [BIG PROJECT] Plots speeding lines on a map of Warsaw (but this time files attached

--- BIG PROJECT/analyse_sth.py
+++ BIG PROJECT/analyse_sth.py
@@ -3,39 +3,51 @@ import json
 from datetime import datetime
 from tools import get_distance
 
-dataSet = 1
+def transform_data_to_simple_dict(data):
+	simple_data = dict()
+	for bus in data['result']:
+		simple_data[bus['VehicleNumber']] = {"Lon": bus['Lon'], "Lat": bus['Lat'], "Time": bus['Time']}
+	return simple_data
 
-path = os.path.join("DATA_SETS", f"DATA_SET_{dataSet}")
+def get_speeding_lines(dataSet=1):
+	speeding_lines = []
+	
+	path = os.path.join("DATA_SETS", f"DATA_SET_{dataSet}")
 
-file_count = sum(1 for item in os.listdir(path) if os.path.isfile(os.path.join(path, item)))
+	file_count = sum(1 for item in os.listdir(path) if os.path.isfile(os.path.join(path, item)))
 
-prev_file_path = os.path.join(path, "0.json")
-prev_file = open(prev_file_path, "r")
-prev_data = json.load(prev_file)
+	prev_file_path = os.path.join(path, "0.json")
+	prev_file = open(prev_file_path, "r")
+	prev_data = transform_data_to_simple_dict(json.load(prev_file))
 
-for i in range(1, file_count):
-	curr_file_path = os.path.join(path, f"{i}.json")
-	curr_file = open(curr_file_path, "r")
-	curr_data = json.load(curr_file)
- 
-	prev = prev_data['result'][0]	
-	curr = curr_data['result'][0]
- 
-	prev_time = datetime.strptime(prev["Time"], '%Y-%m-%d %H:%M:%S')
-	curr_time = datetime.strptime(curr["Time"], '%Y-%m-%d %H:%M:%S')
+	for i in range(1, file_count):
+		curr_file_path = os.path.join(path, f"{i}.json")
+		curr_file = open(curr_file_path, "r")
+		curr_data = transform_data_to_simple_dict(json.load(curr_file))
+	
+		for key in prev_data:
+			prev = prev_data.get(key)
+			curr = curr_data.get(key)
+			if (prev == None or curr == None):
+				continue
+		
+			prev_time = datetime.strptime(prev["Time"], '%Y-%m-%d %H:%M:%S')
+			curr_time = datetime.strptime(curr["Time"], '%Y-%m-%d %H:%M:%S')
 
-	time_diff = curr_time - prev_time
+			time_diff = curr_time - prev_time
+			if (time_diff.seconds == 0):
+				continue
 
-	distance = get_distance((prev["Lat"], prev["Lon"]), (curr["Lat"], curr["Lon"]))
+			distance = get_distance((prev["Lat"], prev["Lon"]), (curr["Lat"], curr["Lon"]))
+			velocity = (distance / time_diff.seconds) * 3.6
+		
+			if (velocity > 50):
+				speeding_lines.append(((prev["Lon"], prev["Lat"]), (curr["Lon"], curr["Lat"])))
 
-
-	print(prev)
-	print(curr)
-	print(time_diff, time_diff.seconds, distance, sep=" | ")
-	print()
- 
+		prev_file.close()
+		prev_file = curr_file
+		prev_data = curr_data
+	
 	prev_file.close()
-	prev_file = curr_file
-	prev_data = curr_data
  
-prev_file.close()
+	return speeding_lines
--- BIG PROJECT/plot_sth.py
+++ BIG PROJECT/plot_sth.py
@@ -1,10 +1,31 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from shapely.geometry import Point, LineString
+import analyse_sth
 
 warsaw_gdf = gpd.read_file('warszawa-dzielnice.geojson')
+
+def line_in_Warsaw(line):
+	return point_in_Warsaw(Point(line[0])) and point_in_Warsaw(Point(line[1]))
+
+def point_in_Warsaw(point):
+	is_inside_warsaw = False
+	for index, district_polygon in warsaw_gdf.iterrows():
+		if point.within(district_polygon['geometry']):
+			is_inside_warsaw = True
+			break
+	return is_inside_warsaw
 
 mapa = warsaw_gdf.plot(figsize=(10, 10), edgecolor='black', linewidth=0.5, color=warsaw_gdf["color"], legend=True)	
 
 mapa.set_title('Miejsca w Warszawie, w których autobusy\n przekraczały prędkość 50 km/h', fontdict={'fontsize': 15, 'fontweight': 'medium'})
+
+speeding_lines = list(filter(line_in_Warsaw, analyse_sth.get_speeding_lines()))
+
+speeding_lines_gdf = gpd.GeoDataFrame(geometry=[LineString([line[0], line[1]]) for line in speeding_lines])
+speeding_lines_gdf = speeding_lines_gdf.set_crs(epsg=4326)
+
+# Plot the speeding lines
+speeding_lines_gdf.plot(ax=mapa, color='red')
 
 plt.show()
